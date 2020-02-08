5338c3a407bd12f9538e5bd72887d7ce172b2e03
Kayo <kayo@kayo.moe>
2020-02-08 00:11:54 -0800

    Compress the page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,194 +6,223 @@
           <img
             alt="Brie and MsBobber Image"
             src="~assets/brie_and_ms_bobber.png"
-            width="250px"
+            width="450px"
           />
-        </div>
-        <div class="col-4">
-          <q-list bordered separator>
-            <q-item>
-              <q-item-section>
-                <q-item-label class="text-h6">
-                  Leaderboard
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-            <div v-for="user in leaderBoard" :key="user">
-              <q-item clickable>
-                <q-item-section>
-                  <q-item-label>
-                    # {{ leaderBoard.indexOf(user) + 1 }} -> {{ user.username }}
-                  </q-item-label>
-                </q-item-section>
-              </q-item>
-            </div>
-          </q-list>
         </div>
       </div>
     </div>
-    <div class="column">
+    <div class="column items-center">
       <div class="row text-h4 q-py-lg">Objective</div>
-      <div>
+      <p>
         Brie is our class pet mouse. She needs to be taken care of and loved!
         It’s your job to make sure she’s fed and is as happy as can be.
+      </p>
+      <div class="col-4">
+        <q-list bordered separator>
+          <q-item>
+            <q-item-section>
+              <q-item-label class="text-h6">
+                Leaderboard
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+          <div v-for="user in leaderBoard" :key="user">
+            <q-item clickable>
+              <q-item-section>
+                <q-item-label>
+                  # {{ leaderBoard.indexOf(user) + 1 }} -> {{ user.username }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </div>
+        </q-list>
       </div>
+
       <div class="row text-h4 q-py-lg">How to Play</div>
-      <p>
-        This is text based game that's played in chat, when Bobber is offline.
-      </p>
-      <p>
-        The currency used to play is called Student Points (SP). These points
-        can be earned in a multitude of ways. One way to earn points is by
-        attending class (tuning into the livestream) on time before it reaches
-        the 10 minute mark. Another way many students get SP is by creating
-        drawings, gifs, videos, music, etc which is stream related. These should
-        be submitted in the class Discord and will eventually be shown on
-        stream, after which the student will receive points!
-      </p>
-      <p>
-        To begin playing, enter any command to get Brie’s attention! You can use
-        SP earned from class to purchase food for Brie. Cheese will always be in
-        stock year round, but other foods are seasonal. Make sure to buy the
-        right food at the right time. Brie will automatically eat any food
-        purchased and increase her affection (and sometimes her bond) with you.
-        If you have no SP to spend, you can feed her a cracker a day which is
-        free of charge but offer the lowest stat boost.
-      </p>
-      <p>
-        Your affection level with Brie will help towards completing Bond
-        Activities with her! Bond Activities will affect your bond level with
-        Brie, and increase how much she loves you! Everyone's bond level
-        contributes to Brie's total happiness, and once her happiness rank
-        reaches certain levels, some special events may occur...
-      </p>
-
-      <div class="column text-h4 q-py-lg">Commands</div>
-      <div class="column q-py-lg">
-        <div class="row text-h5">General</div>
-        <div class="row">
-          <q-list bordered separator>
-            <div
-              v-for="generalCommand in generalCommands"
-              :key="generalCommand"
-            >
-              <q-item clickable>
-                <q-item-section>
-                  <q-item-label>
-                    {{ commandPrefix + generalCommand.name }}
-                  </q-item-label>
-                  <q-item-label caption>
-                    {{ generalCommand.description }}
-                  </q-item-label>
-                </q-item-section>
-              </q-item>
-            </div>
-          </q-list>
+      <div class="row justify-center">
+        <p>
+          This is text based game that's played in chat, when Bobber is offline.
+        </p>
+        <p>
+          The currency used to play is called Student Points (SP). These points
+          can be earned in a multitude of ways. One way to earn points is by
+          attending class (tuning into the livestream) on time before it reaches
+          the 10 minute mark.
+        </p>
+        <p>
+          Another way many students get SP is by creating drawings, gifs,
+          videos, music, etc which is stream related. These should be submitted
+          in the class Discord and will eventually be shown on stream, after
+          which the student will receive points!
+        </p>
+        <p>
+          To begin playing, enter any command to get Brie’s attention! You can
+          use SP earned from class to purchase food for Brie. Cheese will always
+          be in stock year round, but other foods are seasonal. Make sure to buy
+          the right food at the right time.
+        </p>
+        <p>
+          Brie will automatically eat any food purchased and increase her
+          affection (and sometimes her bond) with you. If you have no SP to
+          spend, you can feed her a cracker a day which is free of charge but
+          offer the lowest stat boost.
+        </p>
+        <p>
+          Your affection level with Brie will help towards completing Bond
+          Activities with her! Bond Activities will affect your bond level with
+          Brie, and increase how much she loves you! Everyone's bond level
+          contributes to Brie's total happiness, and once her happiness rank
+          reaches certain levels, some special events may occur...
+        </p>
+      </div>
+      <div class="column text-h4 items-center">Commands</div>
+      <div class="row justify-center q-py-lg">
+        <div class="column items-center">
+          <div class="row text-h5">General</div>
+          <div class="row">
+            <q-list bordered separator>
+              <div
+                v-for="generalCommand in generalCommands"
+                :key="generalCommand"
+              >
+                <q-item clickable>
+                  <q-item-section>
+                    <q-item-label>
+                      {{ commandPrefix + generalCommand.name }}
+                    </q-item-label>
+                    <q-item-label caption>
+                      {{ generalCommand.description }}
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
+              </div>
+            </q-list>
+          </div>
         </div>
       </div>
-
-      <div class="column q-py-lg">
-        <div class="text-h4">Items</div>
+      <div class="row justify-center q-py-lg">
         <div class="row">
-          <p>Buy items to unlock more bonding activities</p>
-        </div>
-        <div class="row">
-          <q-list bordered separator>
-            <div v-for="availableItem in store.items" :key="availableItem">
-              <q-item clickable>
-                <q-item-section>
-                  <q-item-label>
-                    {{ commandPrefix + "buy " + Object.keys(availableItem) }}
-                  </q-item-label>
-                  <q-item-label caption>
-                    Cost:
-                    {{ availableItem[Object.keys(availableItem)].cost }} points
-                  </q-item-label>
-                </q-item-section>
-              </q-item>
+          <div class="column items-center">
+            <div class="text-h5">Items</div>
+            <div class="row">
+              <p class="q-px-lg">Buy items to unlock more bonding activities</p>
             </div>
-          </q-list>
-        </div>
-      </div>
-
-      <div class="column q-py-lg">
-        <div class="text-h5">Gifts</div>
-        <div class="row">
-          <p>
-            Give Brie gifts! These contribute towards your affection with Brie
-          </p>
-        </div>
-        <div class="row">
-          <q-list bordered separator>
-            <div v-for="availableGift in store.gifts" :key="availableGift">
-              <q-item clickable>
-                <q-item-section>
-                  <q-item-label>
-                    {{ commandPrefix + "gift " + Object.keys(availableGift) }}
-                  </q-item-label>
-                  <q-item-label caption>
-                    Cost:
-                    {{ availableGift[Object.keys(availableGift)].cost }} points
-                  </q-item-label>
-                </q-item-section>
-              </q-item>
+            <div class="row">
+              <q-list bordered separator>
+                <div v-for="availableItem in store.items" :key="availableItem">
+                  <q-item clickable>
+                    <q-item-section>
+                      <q-item-label>
+                        {{
+                          commandPrefix + "buy " + Object.keys(availableItem)
+                        }}
+                      </q-item-label>
+                      <q-item-label caption>
+                        Cost:
+                        {{ availableItem[Object.keys(availableItem)].cost }}
+                        points
+                      </q-item-label>
+                    </q-item-section>
+                  </q-item>
+                </div>
+              </q-list>
             </div>
-          </q-list>
-        </div>
-      </div>
+          </div>
 
-      <div class="column q-py-lg">
-        <div class="row text-h5">Bonding</div>
-        <div class="row">
-          <p>
-            Various bonding activities that you can do with Brie, these will
-            contribute to your bond level
-          </p>
-        </div>
-        <div class="row">
-          <q-list bordered separator>
-            <div
-              v-for="bondingCommand in bondingCommands"
-              :key="bondingCommand"
-            >
-              <q-item clickable>
-                <q-item-section>
-                  <q-item-label>
-                    {{ commandPrefix + bondingCommand.name }}
-                  </q-item-label>
-                  <q-item-label caption>
-                    {{ bondingCommand.description }}
-                  </q-item-label>
-                </q-item-section>
-              </q-item>
+          <div class="column items-center ">
+            <div class="text-h5">Gifts</div>
+            <div class="row">
+              <p class="q-px-lg">
+                Give Brie gifts! These contribute towards your affection with
+                Brie
+              </p>
             </div>
-          </q-list>
+            <div class="row">
+              <q-list bordered separator>
+                <div v-for="availableGift in store.gifts" :key="availableGift">
+                  <q-item clickable>
+                    <q-item-section>
+                      <q-item-label>
+                        {{
+                          commandPrefix + "gift " + Object.keys(availableGift)
+                        }}
+                      </q-item-label>
+                      <q-item-label caption>
+                        Cost:
+                        {{ availableGift[Object.keys(availableGift)].cost }}
+                        points
+                      </q-item-label>
+                    </q-item-section>
+                  </q-item>
+                </div>
+              </q-list>
+            </div>
+          </div>
         </div>
-      </div>
+        <div class="row q-py-lg">
+          <div class="row">
+            <div class="column items-center">
+              <div class="row text-h5">Bonding</div>
+              <div class="row">
+                <p class="q-px-lg">
+                  Various bonding activities that you can do with Brie, these
+                  will contribute to your bond level
+                </p>
+              </div>
+              <div class="row">
+                <q-list bordered separator>
+                  <div
+                    v-for="bondingCommand in bondingCommands"
+                    :key="bondingCommand"
+                  >
+                    <q-item clickable>
+                      <q-item-section>
+                        <q-item-label>
+                          {{ commandPrefix + bondingCommand.name }}
+                        </q-item-label>
+                        <q-item-label caption>
+                          {{ bondingCommand.description }}
+                        </q-item-label>
+                      </q-item-section>
+                    </q-item>
+                  </div>
+                </q-list>
+              </div>
+            </div>
 
-      <div class="column q-py-lg">
-        <div class="text-h5">Foods</div>
-        <div class="row">
-          <p>
-            Treats purchased will contribute to Brie's affection with you.
-            Current foods in season:
-          </p>
-        </div>
-        <div class="row">
-          <q-list bordered separator>
-            <div v-for="availableFood in store.foods" :key="availableFood">
-              <q-item clickable>
-                <q-item-section>
-                  <q-item-label>
-                    {{ commandPrefix + "feed " + Object.keys(availableFood) }}
-                  </q-item-label>
-                  <q-item-label caption>
-                    Cost:
-                    {{ availableFood[Object.keys(availableFood)].cost }} points
-                  </q-item-label>
-                </q-item-section>
-              </q-item>
+            <div class="column items-center">
+              <div class="text-h5">Foods</div>
+              <div class="row">
+                <p class="q-px-lg">
+                  Treats purchased will contribute to Brie's affection with you.
+                  Current foods in season:
+                </p>
+              </div>
+              <div class="row">
+                <q-list bordered separator>
+                  <div
+                    v-for="availableFood in store.foods"
+                    :key="availableFood"
+                  >
+                    <q-item clickable>
+                      <q-item-section>
+                        <q-item-label>
+                          {{
+                            commandPrefix + "feed " + Object.keys(availableFood)
+                          }}
+                        </q-item-label>
+                        <q-item-label caption>
+                          Cost:
+                          {{ availableFood[Object.keys(availableFood)].cost }}
+                          points
+                        </q-item-label>
+                      </q-item-section>
+                    </q-item>
+                  </div>
+                </q-list>
+              </div>
             </div>
-          </q-list>
+          </div>
         </div>
       </div>
     </div>
